0ea7ca9cde5e47652e9e01289db390e434d4ed41
kushuu <sonikushu007@gmail.com>
2021-09-19 19:41:37 +0530

    Added a loader:

--- app/src/main/java/com/example/noted/LoginActivity.java
+++ app/src/main/java/com/example/noted/LoginActivity.java
@@ -2,12 +2,14 @@ package com.example.noted;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -28,16 +30,23 @@ public class LoginActivity extends AppCompatActivity {
     public void sign_in_user(View view) {
         EditText passwordEditText = (EditText) findViewById(R.id.password);
         EditText usernameEditText = (EditText) findViewById(R.id.username);
+//        ProgressBar pgsBar = (ProgressBar)findViewById(R.id.loading);
+//        pgsBar.setVisibility(view.GONE);
 
         String sUsername = usernameEditText.getText().toString();
         String sPassword = passwordEditText.getText().toString();
 
         if(!sUsername.equals("") && !sPassword.equals("")) {
-            Toast.makeText(this, "Logging user in...", Toast.LENGTH_SHORT).show();
+            ProgressDialog.show(this, "Loading", "Wait while loading...");
             usernameEditText.setText("");
             passwordEditText.setText("");
+
             Intent i = new Intent(LoginActivity.this, MainNotesPageActivity.class);
             startActivity(i);
+        }
+        else {
+            Toast.makeText(this, "Please fill all the details!", Toast.LENGTH_SHORT).show();
+            return;
         }
     }
 }
--- app/src/main/java/com/example/noted/SignUpActivity.java
+++ app/src/main/java/com/example/noted/SignUpActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -18,5 +20,24 @@ public class SignUpActivity extends AppCompatActivity {
     public void go_to_login(View view) {
         Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
         startActivity(i);
+    }
+
+    public void register_user(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.username);
+        EditText passwordEditText = (EditText) findViewById(R.id.password);
+        EditText cnfPasswordEditText = (EditText) findViewById(R.id.confirmPassword);
+
+        if(usernameEditText.getText().toString().equals("")) {
+            Toast.makeText(this, "username is required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(passwordEditText.getText().toString().equals("")) {
+            Toast.makeText(this, "Password must not be empty!", Toast.LENGTH_SHORT).show();
+        }
+        if(!passwordEditText.getText().toString().equals(cnfPasswordEditText.getText().toString())) {
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 }
