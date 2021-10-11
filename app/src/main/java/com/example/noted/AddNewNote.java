9f82def1ebb75fcad0af13b1afc8dac160351254
kushuu <sonikushu007@gmail.com>
2021-10-11 23:18:17 +0530

    finished images work

--- app/src/main/java/com/example/noted/AddNewNote.java
+++ app/src/main/java/com/example/noted/AddNewNote.java
@@ -1,19 +1,24 @@
 package com.example.noted;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -37,8 +42,9 @@ public class AddNewNote extends AppCompatActivity implements View.OnClickListene
     FirebaseAuth mAuth;
     FirebaseDatabase db;
     DatabaseReference ref;
-    Button add_note;
+    Button add_note, add_img;
     EditText note_string_ET;
+    ImageView imgSection;
 
     FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -48,13 +54,18 @@ public class AddNewNote extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_add_new_note);
         setTitle("Note it down.");
 
-        mAuth = FirebaseAuth.getInstance();
-        note_string_ET = (EditText) findViewById(R.id.note_string);
+
         add_note = (Button) findViewById(R.id.add_new_btn);
         add_note.setOnClickListener(this);
-        db = FirebaseDatabase.getInstance();
 
+        add_img = findViewById(R.id.add_note_img_btn);
+        add_img.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        note_string_ET = (EditText) findViewById(R.id.note_string);
+        db = FirebaseDatabase.getInstance();
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        imgSection = (ImageView) findViewById(R.id.add_note_temp_img);
     }
 
     @Override
@@ -63,6 +74,33 @@ public class AddNewNote extends AppCompatActivity implements View.OnClickListene
             case R.id.add_new_btn:
                 add_note_method();
                 break;
+            case R.id.add_note_img_btn:
+                captureImg();
+                break;
+        }
+    }
+
+    private void captureImg() {
+        if(ContextCompat.checkSelfPermission(AddNewNote.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AddNewNote.this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+        add_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent img_work = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(img_work, 100);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap capture = (Bitmap) data.getExtras().get("data");
+            // setting captured image to ImageView.
+            imgSection.setImageBitmap(capture);
         }
     }
 
